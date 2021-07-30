988be0718984ccd0aa4fdd15dbcb65fdbebecbfb
Valere <valeregbv1@gmail.com>
2021-07-30 12:09:20 +0200

    Ajout de test et création de donnees

--- src/main/java/org/materiel/App.java
+++ src/main/java/org/materiel/App.java
@@ -1,4 +1,21 @@
 package org.materiel;
 
+import org.materiel.models.Categorie;
+import org.materiel.repositories.CategorieRepository;
+import org.materiel.repositories.ClientRepository;
+import org.materiel.repositories.FicheLocationRepository;
+import org.materiel.repositories.MaterielRepository;
+
 public class App {
+    public static void main(String[] args) {
+
+        CategorieRepository categorie = new CategorieRepository();
+        ClientRepository client = new ClientRepository();
+        FicheLocationRepository fichelocation = new FicheLocationRepository();
+        MaterielRepository materiel = new MaterielRepository();
+
+        Categorie categorie1 = new Categorie();
+        categorie1.setCodeCategorie("MI");
+        categorie1.setLibelleCategorie("Materiel Medical");
+    }
 }
